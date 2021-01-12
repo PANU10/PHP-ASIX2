c0c4ee5fbe876b4222d2bad5ae0435b16cc24291
PANU10 <ppatel77@elpuig.xeill.net>
2021-01-12 19:32:49 +0100

    Ejercicio1 Hecho

--- Ejercicio1/aside.php
+++ Ejercicio1/aside.php
@@ -1,10 +1,10 @@
 <?php
 echo ' <aside>
             <ul>
-                <li><a href="http://localhost:3000/Ejercicio1/header.php">Link 1</a></li>
-                <li><a href="http://localhost:3000/Ejercicio1/nav.php" target="_blank">Link 2</a></li>
-                <li><a href="http://localhost:3000/Ejercicio1/aside.php" target="_blank">Link 3</a></li>
-                <li><a href="http://localhost:3000/Ejercicio1/footer.php" target="_blank">Link 4</a></li>
+                <li><a href="http://localhost:3000/Ejercicio1/index.php">Index</a></li>
+                <li><a href="http://localhost:3000/Ejercicio1/contact.php" target="_blank">Contact</a></li>
+                <li><a href="http://localhost:3000/Ejercicio1/altres.php" target="_blank">Altres</a></li>
+                <li><a href="http://localhost:3000/Ejercicio1/footer.php" target="_blank">Link</a></li>
             </ul>
         </aside>';
 ?>
