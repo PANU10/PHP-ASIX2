0821213eadcef1679bbc6638e082aee95ed77b3d
PANU10 <ppatel77@elpuig.xeill.net>
2021-01-13 19:19:45 +0100

    Empezando 5

--- Ejercicio5/index.php
+++ Ejercicio5/index.php
@@ -6,6 +6,16 @@
     <title>Ejercicio 5</title>
 </head>
 <body>
+<p>Tria una opció:</p>
+
+<table>
+	<ul>
+		<li><a href="nom-1.php">Escriu el nom</a></li>
+		<li><a href="cognoms-1.php">Escriu els cognoms</a></li>
+		<li><a href="dades.php">Visualitzar nom i cognoms</a></li>
+		<li><a href="esborrar.php">Esborrar la informació</a></li>
+	</ul>
+</table>
     
 </body>
 </html>
